5856e0ee745b8fc083b43e6c2276a6ef11312e34
Luis Silva <luansimo@outlook.com>
2022-02-03 16:56:41 -0600

    idk at this point

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 import com.lasm.perro.*;
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Hello, World!");
         Dog perro1 = new Dog("Firulais", "café");
         perro1.bark();
